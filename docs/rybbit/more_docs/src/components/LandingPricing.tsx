f2daadbf18146276537181379f46f9e414db4383
Bix von Goeler <bixvongoeler@gmail.com>
2026-08-23 14:27:19 -0400

    remove

--- docs/rybbit/more_docs/src/components/LandingPricing.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useState } from "react";
-import { PricingSection } from "./PricingSection";
-
-export function LandingPricing() {
-  const [isAnnual, setIsAnnual] = useState(true);
-  return <PricingSection isAnnual={isAnnual} setIsAnnual={setIsAnnual} />;
-}
--- docs/rybbit/more_docs/src/i18n/routing.ts
+++
@@ -1,7 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-
-export const routing = defineRouting({
-  locales: ['en', 'de', 'fr', 'zh', 'es', 'pl', 'it', 'ko', 'pt', 'ja'],
-  defaultLocale: 'en',
-  localePrefix: 'as-needed',
-});
